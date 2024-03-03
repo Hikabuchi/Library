de71e6ba7e397a01e80e9edd7e99c533e88901a3
Hikabuchi <tanya_demchuk_isvp@mail.ru>
2024-03-03 12:23:59 +0700

    fix: fix section abaut

--- script.js
+++ script.js
@@ -3,8 +3,26 @@ $(document).ready(function(){
     $('.slider').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
+        arrows:false,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 4000,
         dots: true,
+        dotsClass: 'slick-dots',
+        responsive: [{
+            breakpoint: 1425,
+            settings:{
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+
+        },{
+             breakpoint: 768,
+             settings:{
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+       
+    ]
     });
 });
