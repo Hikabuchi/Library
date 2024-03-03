60ed6f5963a458225665c91ebb120c14f122cf9f
Hikabuchi <tanya_demchuk_isvp@mail.ru>
2024-03-03 11:06:06 +0700

    feat: added  slider functionality

---
+++ script.js
@@ -0,0 +1,10 @@
+//slider
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: true,
+    });
+});
